e82a789c544342cfc047c8e3b4aaf664b03db020
Adriano Costa <adrianocjc@gmail.com>
2021-07-09 10:12:06 -0300

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,17 @@
-#include "holberton.h"
+#include <stdio.h>
+
+int _atoi(char *);
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int _atoi(char *s)
+int main(void)
 {
+	int n;
 
+	n = _atoi("4");
+	printf("%d\n", n);
 	return (0);
 }
